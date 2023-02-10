c3f117635a9d7fd136bf4aab6d629062115e33ba
Jan Ekgardt <omega.punch.2000@gmail.com>
2023-02-10 17:54:08 +0100

    first commit

---
+++ src/main/java/com/example/distancecalculatordb/DistanceCalculatorDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.distancecalculatordb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceCalculatorDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistanceCalculatorDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/distancecalculatordb/DistanceCalculatorDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distancecalculatordb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanceCalculatorDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
