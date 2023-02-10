20435854abde207312514f998f5f5f8baa36e185
Jan Ekgardt <omega.punch.2000@gmail.com>
2023-02-10 17:57:35 +0100

    second commit

--- src/main/java/com/example/distancecalculatordb/DistanceCalculatorDbApplication.java
+++ src/main/java/com/example/distancecalculatordb/DistanceCalculatorDbApplication.java
@@ -1,5 +1,6 @@
 package com.example.distancecalculatordb;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class DistanceCalculatorDbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DistanceCalculatorDbApplication.class, args);
+        System.out.println();
     }
 
 }
